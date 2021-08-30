5602c9888c36a7811aed1d2ad5c31cb9e44d9fbd
Dominik Schenk <domi.schenk@gmail.com>
2021-08-31 00:45:03 +0200

    fix: insert scrollbar into update modal

--- src/app/shared/components/update-modal/update-modal.component.ts
+++ src/app/shared/components/update-modal/update-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, SecurityContext } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, SecurityContext, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { relaunch } from "@tauri-apps/api/process";
 import { installUpdate, UpdateResult } from '@tauri-apps/api/updater';
@@ -9,8 +9,9 @@ import marked from 'marked';
     templateUrl: './update-modal.component.html',
     styleUrls: ['./update-modal.component.scss']
 })
-export class UpdateModalComponent implements OnInit {
+export class UpdateModalComponent implements AfterViewInit {
 
+    @ViewChild('content') content: ElementRef<HTMLElement>;
 
     version: string;
     manifest: string;
@@ -22,9 +23,6 @@ export class UpdateModalComponent implements OnInit {
         this.version = value.manifest.version;
         const manifest = marked(value.manifest.body);
         this.manifest = this.domSanitizer.sanitize(SecurityContext.HTML, manifest);
-        this.manifest += this.manifest;
-        this.manifest += this.manifest;
-        this.manifest += this.manifest;
     }
 
 
@@ -32,7 +30,42 @@ export class UpdateModalComponent implements OnInit {
         private domSanitizer: DomSanitizer
     ) { }
 
-    ngOnInit(): void {
+    ngAfterViewInit(): void {
+        const shadowDom = this.content.nativeElement.shadowRoot;
+        if (shadowDom) {
+            const scrollStyle = `
+                ::-webkit-scrollbar {
+                    height: 7px;
+                    width: 7px;
+                }
+
+                /* Track */
+
+                ::-webkit-scrollbar-track {
+                    background: transparent;
+                }
+
+                /* Handle */
+
+                ::-webkit-scrollbar-thumb {
+                    background: var(--cds-global-color-gray-300);
+                }
+
+                /* Handle on hover */
+
+                ::-webkit-scrollbar-thumb:hover {
+                    background: var(--cds-global-color-gray-600);
+                }
+            `;
+
+            const style = shadowDom.querySelectorAll('style');
+
+            if (!Array.from(style).some((el: any) => el.innerHTML === scrollStyle)) {
+                const scrollCss = document.createElement('style');
+                scrollCss.innerHTML = scrollStyle;
+                shadowDom.insertBefore(scrollCss, shadowDom.firstChild);
+            }
+        }
     }
 
     async startUpdate() {
